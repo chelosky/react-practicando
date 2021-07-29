fb27018d798cfcce70198b811058272ab2df4d39
Chelosky <marcelolazo19972013@hotmail.com>
2021-07-29 14:03:00 -0400

    simple app props

--- 03-primera-app/src/PrimeraApp.js
+++ 03-primera-app/src/PrimeraApp.js
@@ -1,13 +1,23 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-const PrimeraApp = () => {
+const PrimeraApp = ({ saludo, subtitulo}) => {
 
     return (
         <>
-            <h1>Hola Mundo</h1>
-            <h1>Hola Mundo</h1>
+            <h1> {saludo} </h1>
+            {/* <h1>Hola Mundo</h1> */}
+            <p> {subtitulo} </p>
         </>
     );
+}
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Relleno'
 }
 
 export default PrimeraApp;
