4a06afeaef10490f890d24d3dd5c6f3c8996593d
Chelosky <marcelolazo19972013@hotmail.com>
2021-07-29 03:01:49 -0400

    React app 1/3

---
+++ 03-primera-app/src/PrimeraApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PrimeraApp = () => {
+
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <h1>Hola Mundo</h1>
+        </>
+    );
+}
+
+export default PrimeraApp;
